b3de4e915bc90d27f7e857ea317a2058c6f06ac9
mariawenrose <mariarose.wen@gmail.com>
2019-06-25 00:38:42 +1200

    updated clock, added my computer button, added root window media querey

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@ function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
+    var currentHours;
+    
+    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+
     m = checkTime(m);
     document.getElementById('clock').innerHTML =
-    h + ":" + m;
+    h + ":" + m + " " + timeOfDay;
     var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
+}
+
+function checkTime(i) {
     if (i < 10) {i = "0" + i}; 
     return i;
-  }
+}
